02d8ef4e1e99ac94805e9b28c74005729d5660fb
nubrex314 <128887882+nubrex314@users.noreply.github.com>
2024-04-25 21:23:20 +0200

    Update EEPROM.c

--- cviceni 7/EEPROM.c
+++ cviceni 7/EEPROM.c
@@ -8,3 +8,65 @@
 #include <avr/io.h>
 #include <avr/cpufunc.h>
 #include "EEPROM.h"
+#include "timer.h"
+#include "TWI.h"
+#define Addr_eeprom 0x50
+
+void eeprom_write(uint16_t addr,uint8_t *data,uint16_t size) 
+{
+	// Cteni dat z I2C
+	uint16_t i = 0;
+	twi_start();
+	
+	TWDR=(Addr_eeprom<<1)|0;//+write bit
+	twi_addres_write_ack();
+	
+	uint8_t high = (addr&0x1f00)>>8;
+	TWDR=high;
+	twi_data_write_ack();
+	
+	uint8_t low = (addr&0x00ff);
+	TWDR=low;
+	twi_data_write_ack();
+	
+	for (i=0;i<size;i++)
+	{
+		TWDR = data[i];
+		twi_data_write_ack();
+	}
+	twi_stop();
+}
+
+void eeprom_read(uint16_t add,uint8_t *data,uint16_t size)
+{
+	// Cteni dat z I2C
+	uint16_t i = 0;
+	twi_start();
+	
+	TWDR=(Addr_eeprom<<1)|0;//+write bit
+	twi_addres_write_ack();
+	
+	uint8_t high = (add&0x1f00)>>8;
+	TWDR=high;
+	twi_data_write_ack();
+	
+	uint8_t low = (add&0x00ff);
+	TWDR=low;
+	twi_data_write_ack();
+	
+	twi_restart();
+	
+	TWDR=(Addr_eeprom<<1)|1;//+read bit
+	twi_addres_read_ack();
+	
+	
+	for (i=0;i<size;i++)
+	{
+		twi_data_read_ack(1);
+		data[i] =TWDR ;
+		
+	}
+	twi_data_read_ack(0);
+	
+	twi_stop();
+}
