fec345c785ca2a7c0076f2b46423ff9b8062eacc
nubrex314 <128887882+nubrex314@users.noreply.github.com>
2024-04-25 21:24:01 +0200

    Update EEPROM.h

--- cviceni 7/EEPROM.h
+++ cviceni 7/EEPROM.h
@@ -1,11 +1,3 @@
-/*
- * EEPROM.h
- *
- * Created: 20.03.2024 12:51:37
- *  Author: msukd
- */ 
-
-
 #ifndef EEPROM_H_
 #define EEPROM_H_
 
@@ -13,6 +5,9 @@
 #include <avr/interrupt.h>
 
 
+void eeprom_write(uint16_t add,uint8_t *data,uint16_t size);
 
+
+void eeprom_read(uint16_t add,uint8_t *data,uint16_t size);
 
 #endif /* EEPROM_H_ */
